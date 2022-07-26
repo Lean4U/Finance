40137816d90f436b04b03a9f291bf03a880c6aee
Lean4U <108192558+Lean4U@users.noreply.github.com>
2022-07-26 14:51:37 -0500

    Add files via upload

---
+++ Finance.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Simple Stock Price App
+
+Shown are the stock closing price and volume of Amazon!
+
+""")
+
+# http://towardsdatascience.com/how-to-get-stick-data-using-python-c0de1df17e75
+# define the ticker symbol
+
+tickerSymbol = 'AMZN'
+
+# get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+
+# get the historical proces for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+
+# Open High Low Close Volume Dividents Stock Splits
+
+st.write("""
+## Stock Price
+""")
+
+st.line_chart(tickerDf.Close)
+
+st.write("""
+## Stock Volume
+""")
+
+st.line_chart(tickerDf.Volume)
+
+
+
+# In[ ]:
